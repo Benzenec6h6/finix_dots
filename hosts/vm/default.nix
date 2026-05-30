c55be9f9beb32a71fb58e54a86072855ead4af33
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-05-31 03:13:35 +0900

    Enable VM verification using Nix features

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -11,13 +11,9 @@
 
   boot.kernelPackages = pkgs.linuxPackages;
   #boot.kernelPackages = pkgs.linuxPackages_6_12;
-  services.xserver.videoDrivers = ["virtio"];
 
-  # VM特有の設定
-  boot.initrd.kernelModules = ["virtio_pci" "virtio_blk"];
-
-  # QEMU の guest agent を使う場合など
-  services.qemuGuest.enable = true;
+  virtualisation.qemu.extraArgs = ["-vga" "virtio" "-display" "sdl"];
+  virtualisation.diskSize = 4096;
 
   networking.hostName = vars.host;
   system.stateVersion = vars.version;
