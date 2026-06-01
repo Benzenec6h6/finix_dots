8f30ce8b9dbf48f3285bc3e4408864217e56f5e7
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-06-01 18:22:24 +0900

    Import the settings under core

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -6,15 +6,11 @@
   imports = [
     ./hardware.nix
     ./disko.nix
-    #../../modules/core
+    ../../modules/core
   ];
 
   boot.kernelPackages = pkgs.linuxPackages;
   #boot.kernelPackages = pkgs.linuxPackages_6_12;
 
-  virtualisation.qemu.extraArgs = ["-vga" "virtio" "-display" "sdl"];
-  virtualisation.diskSize = 4096;
-
   networking.hostName = vars.host;
-  system.stateVersion = vars.version;
 }
